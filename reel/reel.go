aa7e151bfb2b402cf23d39f07b3b33f2eaf7c1ae
Tobias Gärdebro <tubbeg@gmail.com>
2025-12-27 00:45:30 +0100

    Working on scoring system.

--- reel/reel.go
+++ reel/reel.go
@@ -1,15 +1,19 @@
+components {
+  id: "reel"
+  component: "/reel/reel.script"
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"logo\"\n"
+  data: "default_animation: \"beta\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/main/main.atlas\"\n"
+  "  texture: \"/reel/reel.atlas\"\n"
   "}\n"
   ""
   scale {
-    x: 0.5
-    y: 0.5
+    x: 2.0
+    y: 2.0
   }
 }
