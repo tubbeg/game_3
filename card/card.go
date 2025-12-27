9bb29d836af04e67fcb28f2e484ddb1b061eb56b
Tobias Gärdebro <tubbeg@gmail.com>
2025-12-27 13:12:41 +0100

    New branch

---
+++ card/card.go
@@ -0,0 +1,30 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"back\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/card/card.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 2.0
+    y: 2.0
+  }
+}
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "default_animation: \"one\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/card/card.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 2.0
+    y: 2.0
+  }
+}
