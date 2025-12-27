7d50b4cd3012b9404ae9b6103aa1cf8be1d9fc8e
Tobias Gärdebro <tubbeg@gmail.com>
2025-12-27 18:41:26 +0100

    Working on selection system.

--- card/card.go
+++ card/card.go
@@ -1,22 +1,11 @@
+components {
+  id: "card"
+  component: "/card/card.script"
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"back\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/card/card.atlas\"\n"
-  "}\n"
-  ""
-  scale {
-    x: 2.0
-    y: 2.0
-  }
-}
-embedded_components {
-  id: "sprite1"
-  type: "sprite"
-  data: "default_animation: \"one\"\n"
+  data: "default_animation: \"back_1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
